7e143d878faf01311ae645481f0153288bf88536
Mario Menjívar <mariomenjr@gmail.com>
2020-04-02 00:00:55 -0700

    Manage conditional UI in PartForm

--- src/part/PartController.java
+++ src/part/PartController.java
@@ -1,27 +1,97 @@
 package part;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.RowConstraints;
+import models.InHouse;
+import models.OutSourced;
 import models.Part;
 
 public class PartController {
 
     @FXML
+    private RadioButton PartInHouseRadio;
+    @FXML
+    private RadioButton PartOutsourcedRadio;
+    @FXML
     private TextField PartTextfieldId;
+    @FXML
+    private TextField PartTextfieldName;
+    @FXML
+    private TextField PartTextfieldInv;
+    @FXML
+    private TextField PartTextfieldPrice;
+    @FXML
+    private TextField PartTextfieldMax;
+    @FXML
+    private TextField PartTextfieldMin;
+    @FXML
+    private TextField PartTextfieldCompanyName;
+    @FXML
+    private RowConstraints RowCompanyName;
 
-    private Part partRow;
+    private ToggleGroup _tG;
+    private Part _partRow;
 
-    public PartController() {
-        super();
+    private boolean _isPartOutSourced = false;
+    private boolean _isPartInHouse = false;
+
+    private void _init() {
+        this._tG = new ToggleGroup();
+        this.PartInHouseRadio.setToggleGroup(_tG);
+        this.PartOutsourcedRadio.setToggleGroup(_tG);
+
+        this._tG.selectedToggleProperty().addListener((observable, oldValue, newValue) -> {
+            if ((Toggle) newValue == (Toggle) this.PartInHouseRadio) {
+                _reactToggleGroup("models.InHouse");
+            } else if ((Toggle) newValue == (Toggle) this.PartOutsourcedRadio) {
+                _reactToggleGroup("models.OutSourced");
+            } else {
+            }
+            System.out.println("x");
+        });
+    }
+
+    private void _reactToggleGroup(String className) {
+        switch (className) {
+            case "models.OutSourced":
+                this._tG.selectToggle(this.PartOutsourcedRadio);
+                this._isPartOutSourced = true;
+                this.PartTextfieldCompanyName.setVisible(true);
+                break;
+
+            case "models.InHouse":
+                this._tG.selectToggle(this.PartInHouseRadio);
+                this._isPartInHouse = true;
+                this.PartTextfieldCompanyName.setVisible(false);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void _populateForm() {
+        this.PartTextfieldId.textProperty().set(new Integer(this._partRow.getId()).toString());
+        this.PartTextfieldName.textProperty().set(this._partRow.getName());
+        this.PartTextfieldInv.textProperty().set(new Integer(this._partRow.getStock()).toString());
+        this.PartTextfieldPrice.textProperty().set(this._partRow.getPrice().toString());
+        this.PartTextfieldMax.textProperty().set(new Integer(this._partRow.getMax()).toString());
+        this.PartTextfieldMin.textProperty().set(new Integer(this._partRow.getMin()).toString());
+
+        // this.PartTextfieldCompanyName.textProperty().set(this._partRow.get().toString());
     }
 
     public void setPartRow(Part parRow) {
-        this.partRow = parRow;
-        System.out.println("Set");
+        this._partRow = parRow;
+        this._init();
 
-        this.PartTextfieldId.textProperty().set(new Integer(this.partRow.getId()).toString());
+        this._reactToggleGroup(_partRow.getClass().getName());
+        this._populateForm();
     }
 
     public void onTextfieldKeyTyped(KeyEvent event) {
@@ -32,4 +102,5 @@ public class PartController {
             System.exit(1);
         }
     }
+
 }
